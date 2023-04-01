25a8b5dce9eb5c6b75f0a038b130a4abe5995cb9
Ausar <464270342@qq.com>
2023-04-01 14:54:47 +0800

    好像差不多了
    Signed-off-by: Ausar <464270342@qq.com>

--- source/draw.cpp
+++ source/draw.cpp
@@ -5,6 +5,8 @@
 #include <nds.h>
 #include <nds/ndstypes.h>
 #include <nds/arm9/video.h>
+#include <nds/memory.h>
+#include <nds/arm9/background.h>
 
 
 #include "hzk12.h"
@@ -16,16 +18,15 @@
 //高位为0是透明的（不显示）
 u8 pReadCache [MAX_pReadCache_size];
 
-
-//vram[y][x] vram[256][192]
+//vram[y][x] vram[SCREEN_WIDTH][SCREEN_HEIGHT]
 u16 (*vramTop)[SCREEN_WIDTH] = (u16 (*)[SCREEN_WIDTH])BG_GFX;
 u16 (*vramButton)[SCREEN_WIDTH] = (u16 (*)[SCREEN_WIDTH])BG_GFX_SUB;
 void  setPixel3_main ( int  row ,  int  col ,  u16  color )  { 
-    BG_GFX [ OFFSET ( row ,  col ,  SCREENWIDTH )]  =  color  |  PIXEL_ENABLE ; 
+    BG_GFX [ OFFSET ( row ,  col ,  SCREEN_WIDTH )]  =  color  |  PIXEL_ENABLE ; 
 }
 
 void  setPixel3_sub ( int  row ,  int  col ,  u16  color )  { 
-    BG_GFX_SUB [ OFFSET ( row ,  col ,  SCREENWIDTH )]  =  color  |  PIXEL_ENABLE ; 
+    BG_GFX_SUB [ OFFSET ( row ,  col ,  SCREEN_WIDTH )]  =  color  |  PIXEL_ENABLE ; 
 }
 //******************************************************************************
 void Clear(u16 x, u16 y, u16 w, u16 h, u16 c)
@@ -34,15 +35,15 @@ void Clear(u16 x, u16 y, u16 w, u16 h, u16 c)
 	u16 yi,ww,hh;
     
 
-    hh = (y+h>192)?192:(y+h);
-    ww  = (x+w>256)?(256-x):w;
+    hh = (y+h>SCREEN_HEIGHT)?SCREEN_HEIGHT:(y+h);
+    ww  = (x+w>SCREEN_WIDTH)?(SCREEN_WIDTH-x):w;
 
-	//u16 tmp[256];
-	for(u32 i=0;i<256;i++)
+	//u16 tmp[SCREEN_WIDTH];
+	for(u32 i=0;i<SCREEN_WIDTH;i++)
 		((u16*)pReadCache)[i] = c;
 
 	for(yi=y; yi < hh; yi++)
-		dmaCopy(pReadCache,p+yi*256+x,ww*2);         
+		dmaCopy(pReadCache,p+yi*SCREEN_WIDTH+x,ww*2);         
 }
 //******************************************************************************
 void  ClearWithBG(u16* pbg,u16 x, u16 y, u16 w, u16 h)
@@ -50,11 +51,11 @@ void  ClearWithBG(u16* pbg,u16 x, u16 y, u16 w, u16 h)
 	u16 *p = (u16*)VRAM;;
 	u16 yi,ww,hh;
     
-    hh = (y+h>192)?192:(y+h);
-    ww  = (x+w>256)?(256-x):w;
+    hh = (y+h>SCREEN_HEIGHT)?SCREEN_HEIGHT:(y+h);
+    ww  = (x+w>SCREEN_WIDTH)?(SCREEN_WIDTH-x):w;
 
 	for(yi=y; yi < hh; yi++)
-		dmaCopy(pbg+yi*256+x,p+yi*256+x,ww*2);       
+		dmaCopy(pbg+yi*SCREEN_WIDTH+x,p+yi*SCREEN_WIDTH+x,ww*2);       
 }
 //******************************************************************************
 void  DrawPic(u16 *GFX, u16 x, u16 y, u16 w, u16 h, u8 isTrans, u16 tcolor)
@@ -63,8 +64,8 @@ void  DrawPic(u16 *GFX, u16 x, u16 y, u16 w, u16 h, u8 isTrans, u16 tcolor)
 	u16 c;
 	u16 xi,yi,ww,hh;
 
-  hh = (y+h>192)?192:(y+h);
-  ww  = (x+w>256)?(256-x):w;	
+  hh = (y+h>SCREEN_HEIGHT)?SCREEN_HEIGHT:(y+h);
+  ww  = (x+w>SCREEN_WIDTH)?(SCREEN_WIDTH-x):w;	
 	
 	if(isTrans)
 	{
@@ -73,21 +74,19 @@ void  DrawPic(u16 *GFX, u16 x, u16 y, u16 w, u16 h, u8 isTrans, u16 tcolor)
 			{
 				c = GFX[(yi-y)*w+(xi-x)];
 				if(c!=tcolor)
-					p[yi*256+xi] = c;
+					p[yi*SCREEN_WIDTH+xi] = c;
 			}
 	}
 	else
 	{
 		for(yi=y; yi < hh; yi++)
-			dmaCopy(GFX+(yi-y)*w,p+yi*256+x,w*2); 
+			dmaCopy(GFX+(yi-y)*w,p+yi*SCREEN_WIDTH+x,w*2); 
 	}
 }
 //---------------------------------------------------------------------------------
 
 Cord DrawHZText12(char *str, u16 len, u16 x, u16 y, u16 color,bool top)
 {
-
-	color |= PIXEL_ENABLE;
 	u32 i,l,hi=0;
 	u32 location;
 	u8 cc,c1,c2;
@@ -103,8 +102,8 @@ Cord DrawHZText12(char *str, u16 len, u16 x, u16 y, u16 color,bool top)
 		else
 			l=len;
 
-	if((u16)(len*6)>(u16)(256-x))
-		len=(256-x)/6;
+	if((u16)(len*6)>(u16)(SCREEN_WIDTH-x))
+		len=(SCREEN_WIDTH-x)/6;
     while(hi<l)
     {
 		c1 = str[hi];
@@ -115,7 +114,7 @@ Cord DrawHZText12(char *str, u16 len, u16 x, u16 y, u16 color,bool top)
 		}
     	else if(c1<0x80)  //ASCII
     	{
-			if(x>=256-6){
+			if(x>=SCREEN_WIDTH-6){
 				x = 0;
 				y += 12;
 			}
@@ -123,7 +122,7 @@ Cord DrawHZText12(char *str, u16 len, u16 x, u16 y, u16 color,bool top)
     		for(i=0;i<12;i++)//字符的高是12，所以画12次
 			{
 				cc = ASC_DATA[location+i];
-				auto vram_line = &vramTop[y+i][x];
+				auto vram_line = &vram[y+i][x];
 				for(int j=0;j<8;j++){//如果这个字符在第i位是1，那么把对应内存的位置填上color
 					if(cc & (1<<j))
 						vram_line[7-j] = color;
@@ -133,7 +132,7 @@ Cord DrawHZText12(char *str, u16 len, u16 x, u16 y, u16 color,bool top)
     	}
 		else	//Double-byte
 		{	
-			if(x>=256-12){
+			if(x>=SCREEN_WIDTH-12){
 				x = 0;
 				y += 12;
 			}
@@ -149,7 +148,7 @@ Cord DrawHZText12(char *str, u16 len, u16 x, u16 y, u16 color,bool top)
 			for(i=0;i<12;i++)
 			{				
 				cc = acHZK12[location+i*2];
-				auto vram_line = &vramTop[y+i][x];
+				auto vram_line = &vram[y+i][x];
 				for(int j=0;j<8;j++){//第一部分
 					if(cc & (1<<j))
 						vram_line[7-j] = color;
@@ -170,14 +169,21 @@ Cord DrawHZText12(char *str, u16 len, u16 x, u16 y, u16 color,bool top)
 //---------------------------------------------------------------------------------
 
 static char str[128];
-static Cord current_cord={0,0};
+static Cord current_cord_Top={0,0};
+static Cord current_cord_Button={0,0};
 void printf_zh(bool isTop,const char *format, ...)
 {
     va_list va;
     va_start(va, format);
     vsprintf(str, format, va);
     va_end(va);
-    current_cord = DrawHZText12(str,0,current_cord.x,current_cord.y, ARGB16(1,31,31,31),isTop);
+	auto cord = isTop ? current_cord_Top : current_cord_Button;
+    cord = DrawHZText12(str,0,cord.x,cord.y, ARGB16(1,31,31,31),isTop);
+	if(isTop){
+		current_cord_Top = cord;
+	}else{
+		current_cord_Button = cord;
+	}
 	//英文字库结构  12*8 ，高12，宽6 ，前6bit，中文则是宽12
 }
 void printf_zh_color(bool isTop,u16 color,const char *format, ...)
@@ -186,6 +192,34 @@ void printf_zh_color(bool isTop,u16 color,const char *format, ...)
     va_start(va, format);
     vsprintf(str, format, va);
     va_end(va);
-    current_cord = DrawHZText12(str,0,current_cord.x,current_cord.y, color,isTop);
+    auto cord = isTop ? current_cord_Top : current_cord_Button;
+    cord = DrawHZText12(str,0,cord.x,cord.y, color,isTop);
+	if(isTop){
+		current_cord_Top = cord;
+	}else{
+		current_cord_Button = cord;
+	}
+}
+void vramInit(){
+	#define BG_15BITCOLOR    (1<<7)
+	#define BG_CBB1          (1<<2)
+	//2-3   Character Base Block  (0-3, in units of 16 KBytes) (=BG Tile Data)
+	// 7     Colors/Palettes       (0=16/16, 1=SCREEN_WIDTH/1)
+	//14-15 Screen Size (0-3) 
+	//https://www.pmallory.com/bitmap-graphics-on-the-nintendo-ds-part-2-mode-3.html
+	videoSetMode(MODE_3_2D | DISPLAY_BG3_ACTIVE);
+    videoSetModeSub(MODE_3_2D | DISPLAY_BG3_ACTIVE);
+
+    REG_BG3CNT =  BgSize_R_256x256 | BG_15BITCOLOR | BG_CBB1;
+    REG_BG3CNT_SUB = BgSize_R_256x256 | BG_15BITCOLOR | BG_CBB1;
+
+
+    // Don't scale bg3 (set its affine transformation matrix to [[1,0],[0,1]])
+    //在这个模式下不要缩放
+    REG_BG3PA = 1 << 8;
+    REG_BG3PD = 1 << 8;
+
+    REG_BG3PA_SUB = 1 << 8;
+    REG_BG3PD_SUB= 1 << 8;
 }
 //---------------------------------------------------------------------------------
--- source/draw.h
+++ source/draw.h
@@ -1,5 +1,8 @@
 #include <nds.h>
-
+typedef enum{
+	TOP = true,
+	BUTTON = false
+}SCREEN_CHOOSE;
 
 struct Cord{
 	u16 x,y;
@@ -8,14 +11,11 @@ struct Cord{
 #define RGB(r,g,b)  ((r) | (g)<<5 | (b)<<10)
 #define OFFSET(r,c,w) ((r)*(w)+(c))
 
-#define SCREENWIDTH  (256)
-#define SCREENHEIGHT (192)
-
 #define REG_DISPCNT_MAIN  (*(vu32*)0x04000000)
 #define REG_DISPCNT_SUB   (*(vu32*)0x04001000)  
-
 void Clear(u16 x, u16 y, u16 w, u16 h, u16 c);
 void ClearWithBG(u16* pbg,u16 x, u16 y, u16 w, u16 h);
 void DrawPic(u16 *GFX, u16 x, u16 y, u16 w, u16 h, u8 isTrans, u16 tcolor);
 void printf_zh(bool isTop,const char *format, ...);
 void printf_zh_color(bool isTop,u16 color,const char *format, ...);
+void vramInit();
